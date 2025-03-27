df24667aa5aae7265325dd63171ac1360e7afd2c
Felipe <cl204218@g.unicamp.br>
2025-03-27 17:46:39 -0300

    Atualizando Cors 2

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -32,21 +32,14 @@ export default db;
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://find-it-adm.vercel.app", "https://find-it-user.vercel.app"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://find-it-adm.vercel.app",
+  "https://find-it-user.vercel.app",
+];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 
